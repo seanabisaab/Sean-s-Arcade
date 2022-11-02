b00b9e35fbe4731c2e799e7ff7784d3e5768aca5
Sean <seanabisaab2002@gmail.com>
2022-11-01 18:34:39 -0700

    new commit

--- docs/js/match.js
+++ docs/js/match.js
@@ -1,16 +1,3 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
-
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});
-
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
-
 document.addEventListener('DOMContentLoaded', () => {
 const cardArray = [
     {
