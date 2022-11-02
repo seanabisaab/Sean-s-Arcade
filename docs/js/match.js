5f81d02020170d7f67d021188fe484bbf6df118b
Sean <seanabisaab2002@gmail.com>
2022-11-02 00:55:18 -0700

    completed matching game

--- docs/js/match.js
+++ docs/js/match.js
@@ -1,3 +1,16 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
+
 document.addEventListener('DOMContentLoaded', () => {
   //card options
   const cardArray = [
